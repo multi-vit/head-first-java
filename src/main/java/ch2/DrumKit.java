1eac9e305613ac190db1b153796c644adcfed19f
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-04-27 16:42:36 +0100

    Add chapter 2 DrumKit

---
+++ src/main/java/ch2/DrumKit.java
@@ -0,0 +1,12 @@
+package ch2;
+
+public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+    void playTopHat () {
+        System.out.println("ding ding da-ding");
+    }
+    void playSnare () {
+        System.out.println("bang bang ba-bang");
+    }
+}
---
+++ src/main/java/ch2/DrumKitTestDrive.java
@@ -0,0 +1,13 @@
+package ch2;
+
+public class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        d.playTopHat();
+        if(d.snare == true) {
+            d.playSnare();
+        }
+    }
+}
