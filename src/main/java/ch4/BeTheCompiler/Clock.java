dc3597334187855f07652d43c9aaa818eaf4445d
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-05-08 10:43:15 +0100

    Add ch4 compiler exercise 2 solution

---
+++ src/main/java/ch4/BeTheCompiler/Clock.java
@@ -0,0 +1,28 @@
+package ch4.BeTheCompiler;
+
+// Won't compile
+// Getter on line 15 is currently set to return nothing
+// So needs changing from "void" to "String" as the return type
+// If that is fixed, output will be:
+// time: 1245
+public class Clock {
+    String time;
+
+    void setTime(String t) {
+        time = t;
+    }
+
+    void getTime() {
+        return time;
+    }
+}
+
+class ClockTestDrive {
+    public static void main(String[] args) {
+        Clock c = new Clock();
+
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("time: " + tod);
+    }
+}
