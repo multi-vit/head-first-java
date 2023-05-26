a27882b926d5c56a9ada4ff79194fa2d221229b4
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-05-26 08:51:12 +0100

    Add my attempt at the SimpleStartUpGame class implementation

---
+++ src/main/java/ch5/SimpleStartUpGame.java
@@ -0,0 +1,34 @@
+package ch5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class SimpleStartUpGame {
+    public static void main(String[] args) {
+        int numUserGuesses = 0;
+        boolean isAlive = true;
+        int[] locations = new int[3];
+        SimpleStartup game = new SimpleStartup();
+        Scanner in = new Scanner(System.in);
+        Random r = new Random();
+        int randomNumber = r.nextInt(4) + 1;
+        System.out.println("Random number is: " + randomNumber);
+        for (int i = 0; i < locations.length; i++) {
+            locations[i] = randomNumber;
+            randomNumber++;
+        }
+        System.out.println("Locations are: " + locations[0] + locations[1] + locations[2]);
+        game.setLocationCells(locations);
+        while (isAlive) {
+            System.out.println("Please guess a location which will be a number between 1 and 7 (inclusive)");
+            int userGuess = in.nextInt();
+            String result = game.checkYourself(userGuess);
+            numUserGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numUserGuesses + " guesses");
+            }
+        }
+    }
+
+}
