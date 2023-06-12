e527e36e301434e5ecdf2ff4886852b8bc44119f
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-06-12 15:14:04 +0100

    Update Startup to return result directly

--- src/main/java/ch6/SinkAStartup/Startup.java
+++ src/main/java/ch6/SinkAStartup/Startup.java
@@ -4,24 +4,34 @@ import java.util.ArrayList;
 
 public class Startup {
 
+    private String name;
     private ArrayList<String> locationCells;
 
     public void setLocationCells(ArrayList<String> locs) {
         locationCells = locs;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String checkYourself(String userInput) {
-        String result = "miss";
         int index = locationCells.indexOf(userInput);
 
         if (index >= 0) {
             locationCells.remove(index);
+            System.out.println("Remaining locations are: " + String.join(", ", locationCells));
             if (locationCells.isEmpty()) {
-                result = "kill";
+                return "kill";
             } else {
-                result = "hit";
+                return "hit";
             }
         }
-        return result;
+        System.out.println("Remaining locations are: " + String.join(", ", locationCells));
+        return "miss";
     }
 }
