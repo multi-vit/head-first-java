61c04700751c7e0213650bf0df598fde53e06413
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-06-12 15:31:16 +0100

    Add official Startup class solution

---
+++ src/main/java/ch6/SinkAStartup/OfficialSolution/Startup.java
@@ -0,0 +1,31 @@
+package ch6.SinkAStartup.OfficialSolution;
+
+import java.util.ArrayList;
+
+public class Startup {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! You sunk " + name + "   : ( ");
+            } else {
+                result = "hit";
+            } // end if
+        } // end outer if
+        return result;
+    } // end method
+} // close class
