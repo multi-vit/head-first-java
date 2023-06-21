b765ed6b0ca0e8af127996d1ded4fb7a84f9357e
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-06-21 16:05:40 +0100

    Add ch6 CodeMagnet solution

---
+++ src/main/java/ch6/CodeMagnets/ArrayListMagnet.java
@@ -0,0 +1,44 @@
+package ch6.CodeMagnets;
+
+import java.util.ArrayList;
+
+public class ArrayListMagnet {
+    public static void printList(ArrayList<String> list) {
+        for (String element : list) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add(0, "zero");
+        a.add(1, "one");
+        a.add(2, "two");
+        a.add(3, "three");
+        printList(a);
+        if (a.contains("three")) {
+            a.add("four");
+        }
+        a.remove(2);
+        printList(a);
+        if (a.indexOf("four") != 4) {
+            a.add(4, "4.2");
+        }
+        printList(a);
+        if (a.contains("two")) {
+            a.add("2.2");
+        }
+        printList(a);
+    }
+}
+
+
+
+
+
+
+
+
+
+
